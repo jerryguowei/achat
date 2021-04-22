930fcc2afe8f71c96ad50d7727e4dd8a5fd21b83
jerryguowei <jerryguowei@gmail.com>
2021-04-22 12:34:53 +1000

    asd
    ad

--- server/src/main/java/com/duduanan/achat/entity/UserInfo.java
+++ server/src/main/java/com/duduanan/achat/entity/UserInfo.java
@@ -1,17 +1,27 @@
 package com.duduanan.achat.entity;
 
-import javax.persistence.*;
-
-import org.hibernate.annotations.NaturalId;
-
-import com.duduanan.achat.dto.UserRegistractionDTO;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.NaturalId;
+
+import com.duduanan.achat.dto.UserRegistractionDTO;
 
 @Entity(name = "user_info")
 public class UserInfo {
